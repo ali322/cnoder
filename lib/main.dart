7ab4f816d9a702e36b1803f98ddda4f8e8ca0e7b
alichen <ali322@gmail.com>
2020-02-28 16:26:41 +0800

    fix pass

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 import "./app.dart";
 import "./store/index.dart";
 
-void main() async{
+void main() async {
   final _store = await loadStore();
   runApp(new App(store: _store));
 }
